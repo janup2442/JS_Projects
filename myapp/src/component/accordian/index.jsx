0a809eb70cbf1e33f0509cae3fb169b60a2b815f
Anup Jaiswal <janup2442@gmail.com>
2024-12-21 10:05:31 +0000

    multiselection accordian done

--- myapp/src/component/accordian/index.jsx
+++ myapp/src/component/accordian/index.jsx
@@ -1,11 +1,45 @@
-// single selection accoridan
+// multi selection accordian
 
 
+import {data} from "./data"
+import "./style.css"
+import { useState } from "react"
+import ArrowDropDownOutlinedIcon from '@mui/icons-material/ArrowDropDownOutlined';
+import ArrowDropUpOutlinedIcon from '@mui/icons-material/ArrowDropUpOutlined';
 export default function Accordian(){
-    const [selected,setSelected] = useState(null);
     return(
         <>
-            <div>anup</div>
+            <div className="container">
+                {
+                    (data && data.length>0)? 
+                    data.map(item=>(
+                        <Item key={item.id} id={item.id}  title={item.question} info={item.answer}/>
+                    ))
+                    :(<p>No data found</p>)
+                }
+            </div>
         </>
-    );
+    )
+}
+
+function Item(props){
+    const [selected , select] = useState(null);
+    function handleClick(item){
+        console.log(item)
+        selected==null?select(1):select(null);
+    }
+    return(
+        <>
+           <div className="itemRow" onClick={()=>handleClick(props.id)}>
+            <p>{props.id}</p>
+            <p>{props.title}</p>
+            {
+                (selected==null)? (<ArrowDropDownOutlinedIcon/>) : (<ArrowDropUpOutlinedIcon/>)
+            }
+            {
+                selected!=null? (<p className="itemInfo">{props.info}</p>):null
+            }
+           </div>
+        </>
+    )
 }
